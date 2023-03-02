3e5d94949857c90943f2384f2ed8a85adeb65854
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2023-03-02 16:39:31 +0000

    9章まで

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: "homes#top"
+  devise_for :users
+  
+  # get 'postimages/new'
+  # get 'postimages/index'
+  # get 'postimages/show'
+  resources :post_images, only: [:new, :index, :show]
+  
+  get "homes/about" => "homes#about", as: "about"
 end
