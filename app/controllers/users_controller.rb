54fda9fc1d3f3e8ef84f0916c70d865ef0cf320c
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2023-03-08 16:16:16 +0000

    他のユーザーからのアクセス制限まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  # 他のユーザーからのアクセスを制限
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     # @post_images = @user.post_images  #ページネーションの為、下記に変更
@@ -21,4 +25,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image)
   end
 
+  # 他のユーザーからのアクセスを制限するメソッドを作成
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_image_path
+    end
+  end
 end
